1b94e4fab71974a92d8c835127a6bfdbe43bd5ce
VishwaVikas2006 <vishwavikas2006@gmail.com>
2025-08-20 00:34:23 +0530

    Fix asset paths and prepare for Pages deployment

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const BlogItem = ({ blog, onSelect }) => (
   <div className="blog-item">
-<img src={blog.thumbnail} alt={blog.title} className="thumbnail" />
+<img
+  src={process.env.PUBLIC_URL + '/' + blog.thumbnail}
+  alt={blog.title}
+  className="thumbnail"
+/>
+
 
     <div className="blog-info">
       <h2>{blog.title}</h2>
